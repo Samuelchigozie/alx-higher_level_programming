826b948cb4e2b87d0a6c285fc8fce8ce8e0fd149
Samuel Chigozie <nzekwesammy@gmail.com>
2023-05-09 17:25:34 -0700

    Task 14 - task 16 -Advanced

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Author - Samuel Chigozie
+
+def magic_calculation(a, b, c):
+    """Does same as python bytecode provided by ALX."""
+    if a < b:
+        return (c)
+    if c > b:
+        return (a + b)
+    return (a*b - c)
