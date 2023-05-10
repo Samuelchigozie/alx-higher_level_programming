93711c78b4ff86d72732eeba2f1b45357489f18e
Samuel Chigozie <nzekwesammy@gmail.com>
2023-05-09 17:43:13 -0700

    Task 12 update

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,7 +3,7 @@
 """Output numbers 1 to 100 with a space as a separator.
 If a number is divisible by 3, print "Fizz" instead of the number.
 If a number is divisible by 5, print "Buzz" instead of the number.
-If a number is divisible by both 3 and 5, print "FizzBuzz" instead of the number.
+If a num is divisible by both 3 & 5, print "FizzBuzz" than num.
 """
 
 
